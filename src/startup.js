270314f56c6903fbbf7a24b2ad9bfae6f92f554e
nathan6am <nathanpho47@gmail.com>
2022-03-25 12:27:02 -0400

    run crawler on startup if chached odds is empty array

--- src/startup.js
+++ src/startup.js
@@ -5,13 +5,12 @@ import { refreshOdds } from "./queues/scraperQueue.js";
 
 export default async function startup() {
   const currentOdds = await getOdds();
-  if (!currentOdds) {
+  if (!currentOdds || currentOdds.length < 1) {
     const scrapedData = await scrapeOdds();
     const updatedOdds = await parseOddsData(scrapedData);
     await cacheOdds(updatedOdds);
     console.log("intial odds cached");
   } else {
-    console.log(currentOdds)
     console.log("odds already cached");
   }
   refreshOdds();
